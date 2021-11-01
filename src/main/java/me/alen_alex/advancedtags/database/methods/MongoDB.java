7a998e21245fad5413c4f1b322ed43a245a4f6a1
AlenGeoAlex <71920951+AlenGeoAlex@users.noreply.github.com>
2021-11-01 22:38:23 +0530

    Working on database

---
+++ src/main/java/me/alen_alex/advancedtags/database/methods/MongoDB.java
@@ -0,0 +1,11 @@
+package me.alen_alex.advancedtags.database.methods;
+
+import me.alen_alex.advancedtags.database.StorageWorker;
+
+public class MongoDB implements StorageWorker {
+
+    @Override
+    public boolean init() {
+        return false;
+    }
+}
